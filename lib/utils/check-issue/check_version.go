bac508d04b8d00f933f91ddff2741da0b9469449
Yad Smood <y.s.inside@gmail.com>
2022-05-19 16:47:09 +0800

    optimize issue content check

---
+++ lib/utils/check-issue/check_version.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+
+	"github.com/go-rod/rod/lib/utils"
+	"github.com/ysmood/gson"
+)
+
+func checkVersion(body string) error {
+	m := regexp.MustCompile(`\*\*Rod Version:\*\* v[0-9.]+`).FindString(body)
+	if m == "" || m == "**Rod Version:** v0.0.0" {
+		return fmt.Errorf(
+			"Please add a valid `**Rod Version:** v0.0.0` to your issue. Current version is %s",
+			currentVer(),
+		)
+	}
+
+	return nil
+}
+
+func currentVer() string {
+	q := req("/repos/go-rod/rod/tags?per_page=1")
+	res, err := http.DefaultClient.Do(q)
+	utils.E(err)
+	defer func() { _ = res.Body.Close() }()
+
+	data, err := ioutil.ReadAll(res.Body)
+	utils.E(err)
+
+	currentVer := gson.New(data).Get("0.name").Str()
+
+	return currentVer
+}
+
+func req(u string) *http.Request {
+	r, err := http.NewRequest(http.MethodGet, "https://api.github.com"+u, nil)
+	utils.E(err)
+	r.Header.Add("Authorization", "token "+githubToken)
+	return r
+}
